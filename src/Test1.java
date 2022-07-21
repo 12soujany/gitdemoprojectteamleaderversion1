cb52faeec2f76c6b75bad26347a4f7e513d94669
NageshN9901 <nagesh.n8991@gmail.com>
2022-07-22 01:04:56 +0530

    Added additional checks,,.

--- src/Test1.java
+++ src/Test1.java
@@ -4,7 +4,7 @@ public class Test1 {
 public static void main(String[] args) {
 	Scanner sc=new Scanner (System.in);
 	String hungry =sc.next();
-	if(hungry == "yes")
+	if(hungry == "yes" || hungry =="Y" ||hungry=="y")
 		System.out.println("eqat Something");
 	else {
 		System.out.println(" lets contune work");
